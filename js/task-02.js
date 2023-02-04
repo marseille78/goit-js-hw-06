f7100e607745802f66f9d8a49184891c9584277b
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-02-05 01:00:17 +0200

    task-02 was done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = ingredients.map(createItem);
+  document
+    .querySelector("#ingredients")
+    .append(...elements);
+});
+
+function createItem(text) {
+  const elementUI = document.createElement("li");
+  elementUI.textContent = text;
+  return elementUI;
+}
