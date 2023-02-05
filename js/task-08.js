55479fcb7709958edfdf6c64e49938f4ca9c2820
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-02-05 13:59:41 +0200

    task-08 was done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const formUI = document.querySelector(".login-form");
 
+    formUI.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const listFields = Array.from(formUI.elements).filter(item => item.hasAttribute("name"));
+
+        if (!isValidateForm(listFields)) {
+            alert("All fields must be filled");
+            return;
+        }
+
+        handleSubmit(listFields);
+    });
+});
+
+function isValidateForm(fields) {
+    return fields.every(item => item.value.length > 0);
+}
+
+function handleSubmit(fields) {
+    const result = {};
+
+    for (const item of fields) {
+        result[item.name] = item.value;
+    }
+
+    console.log(result);
+
+    clearForm(fields);
+}
+
+function clearForm(fields) {
+    for (const item of fields) {
+        item.value = "";
+    }
+}
