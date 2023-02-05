e1b9dee7929b177964557e07783aea171d4cd64d
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-02-05 14:22:27 +0200

    task-09 was done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  (function () {
+    const btnGenerateBgColorUI = document.querySelector(".change-color");
+
+    btnGenerateBgColorUI.addEventListener('click', setBgColor);
+  })();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
 }
